ad819297efc3221fa85c6c7314b017962bfce2c0
Abbott Au <abbott.wu@ixdigit.com>
2018-08-14 14:46:38 +0800

    #

--- service-zuul/src/main/java/org/boot/ZuulApplication.java
+++ service-zuul/src/main/java/org/boot/ZuulApplication.java
@@ -1,6 +1,7 @@
 package org.boot;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ZuulApplication {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+		
+		SpringApplication app = new SpringApplication(ZuulApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
 		System.out.println("====GatewayzuulApplication start successful==========");
 	}
 }
