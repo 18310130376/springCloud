2ae4e6985604e294a307c2879245fbf2805b1fe6
Abbott Au <abbott.wu@ixdigit.com>
2018-08-16 17:58:15 +0800

    #

--- service-zuul/src/main/java/org/boot/security/AuthServiceImpl.java
+++ service-zuul/src/main/java/org/boot/security/AuthServiceImpl.java
@@ -7,17 +7,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 	private AuthenticationManager authenticationManager;
+	
 	private UserDetailsService userDetailsService;
 	private JwtTokenUtil jwtTokenUtil;
 
